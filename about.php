51effa7d9598f0df5203a911d50d08c23584a88d
wilson25616 <68517859+wilson25616@users.noreply.github.com>
2022-10-31 19:44:10 -0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php
+    $title ="Bienveidos ";
+    include "head.php";
+    include "sidebar.php";  
+?>
+
+<div class="right_col" role="main"> <!-- page content -->
+        <div class="">
+            <div class="page-title">
+            <br>
+                <div class="row">
+            	    <div class="col-md-6 col-md-offset-3">
+                        <h1>Desarrollo Web</h1>
+                         <h2>Proyecto Final</h2>
+                        <br><br>
+
+                        <h3>Integrantes</h3>
+                        <p>Jose David Teletor Rosales.</p>
+                        <p>Edgar David Luis Garcia.</p>
+                        <p>Wilson Aldair Aguilar Samayoa.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /page content -->
+
+<?php include "footer.php" ?>
